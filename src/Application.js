895f398b9a1a1349e9f61d7c20713c000c54ef7d
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-12 21:38:29 +0545

    changes

--- src/Application.js
+++ src/Application.js
@@ -3,7 +3,33 @@ import React, {Component} from  'react';
 
 class Application extends Component {
 
+    constructor(props){
+        super(props);
+    }
+    componentWillMount(props, state){
+
+    }
+    componentDidMount(props,state){
+        console.log("Mounted with",props, state);
+    }
+    ComponentWillReceiveProps(props){
+
+    }
+    componentWillUpdate(props, state){
+        if(this.props.name!== props.name){
+            //do something
+        }
+    }
+    componentDidUpdate(props, state){
+        
+    }
+
+
+
+
+
 render(){
+    let name="Santoshi";
     return (
         <div>
         <h1>Hello, World!!!</h1>
